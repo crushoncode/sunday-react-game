3438a38ccd65a988f2252490ad4f01724d689843
Serina Ko <34416116+crushoncode@users.noreply.github.com>
2018-08-05 17:39:48 +1000

    change the Square to be a functional component

--- src/Square.js
+++ src/Square.js
@@ -4,14 +4,16 @@ import React from 'react';
 // passing a function as the onClick prop. It only fires after a click.
 // if we forget () =>, it would fire the alert every time the component re-renders.
 
-class Square extends React.Component {
-  render() {
-    return (
-      <button className="square" onClick={() => this.props.onClick()}>
-        {this.props.value}
-      </button>
-    );
-  }
+// <button className="square" onClick={() => this.props.onClick()}>
+// In a class, we used an arrow function to access the correct this value,
+// but in a functional component we don’t need to worry about this.
+
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
 }
 
 export { Square };
