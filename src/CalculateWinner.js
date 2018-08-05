efd8cc2dae8c1110a048c7a688ca17b74a4a2dea
Serina Ko <34416116+crushoncode@users.noreply.github.com>
2018-08-05 21:29:23 +1000

    add this helper function to determine a winner

---
+++ src/CalculateWinner.js
@@ -0,0 +1,24 @@
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    // && is checking for null values.
+    // === is comparing the values of [a] , [b] , and [c] to see if they are all 'X''s or all 'O''s
+    // return squares[a] can only be 'X' or 'O' after passing the if statement.
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
+export { calculateWinner };
