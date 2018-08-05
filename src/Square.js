f087db7495bccf5286a510246548ddc7aeac7f84
Serina Ko <34416116+crushoncode@users.noreply.github.com>
2018-08-05 16:21:33 +1000

    create an alert for clicking on a Square

--- src/Square.js
+++ src/Square.js
@@ -1,8 +1,16 @@
 import React from 'react';
 
+// <button className="square" onClick={() => alert('click')}>
+// passing a function as the onClick prop. It only fires after a click.
+// if we forget () =>, it would fire the alert every time the component re-renders.
+
 class Square extends React.Component {
   render() {
-    return <button className="square">{this.props.value}</button>;
+    return (
+      <button className="square" onClick={() => alert('click')}>
+        {this.props.value}
+      </button>
+    );
   }
 }
 
