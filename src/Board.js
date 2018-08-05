d2368602e4f0cd4cdb4eac179099c88c30ec724e
Serina Ko <34416116+crushoncode@users.noreply.github.com>
2018-08-05 17:42:13 +1000

    set the first move to be 'X' by default

--- src/Board.js
+++ src/Board.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import { Square } from './Square';
 
+// Since the Square components no longer maintain state,
+// the Square components receive values from the Board component and
+// inform the Board component when they’re clicked. In React terms,
+// the Square components are now controlled components.
+// The Board has full control over them.
+
 class Board extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      squares: Array(9).fill(null)
+      squares: Array(9).fill(null),
+      xIsNext: true
     };
   }
+
+  handleClick(i) {
+    // call .slice() to create a copy of the squares array to modify
+    // The original array will not be modified.
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({ squares: squares });
+  }
+
   renderSquare(i) {
     return (
       <Square
@@ -44,3 +60,11 @@ class Board extends React.Component {
 }
 
 export { Board };
+
+// Immutability makes complex features much easier to implement.
+// Later implement a “time travel” feature that allows us to review
+// the tic-tac-toe game’s history and “jump back” to previous moves.
+// This functionality isn’t specific to games — an ability to undo
+// and redo certain actions is a common requirement in applications.
+// Avoiding direct data mutation lets us keep previous versions of
+// the game’s history intact, and reuse them later.
